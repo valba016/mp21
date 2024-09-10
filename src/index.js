1ed502a25eeeb33e518f81968cf0cf44ee9b43ec
valba016 <qualifiedtypists@gmail.com>
2024-09-10 20:25:16 +1000

    new project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Navbar } from "./components/layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Notfound from "./pages/Notfound";
+import Productlist from "./pages/admin/products/Productlist";
+import Createproduct from "./pages/admin/products/Createproduct";
+import { Footer } from "./pages/Footer";
+import Home from "./pages/Header";
+import Profile from "./pages/admin/products/Profile";
+import About from "./pages/About";
+import Createuser from "./pages/admin/products/Createuser";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/about" element={<About />} />
+        <Route path="/admin/products" element={<Productlist />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin/products/create" element={<Createproduct />} />
+        <Route path="/admin/products/createuser" element={<Createuser />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Notfound.js
@@ -0,0 +1,7 @@
+export default function Notfound() {
+  return (
+    <div className="container my-4">
+      <h2> PAGE NOT FOUND</h2>
+    </div>
+  );
+}
